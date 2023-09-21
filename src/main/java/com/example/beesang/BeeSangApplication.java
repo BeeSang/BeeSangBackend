bda6dbc1b1935bbeeb57ef70c6efdf451c7b1137
zzzzseong <jung48182@gmail.com>
2023-09-21 20:07:40 +0900

    initial commit

---
+++ src/main/java/com/example/beesang/BeeSangApplication.java
@@ -0,0 +1,13 @@
+package com.example.beesang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeeSangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeeSangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/beesang/BeesangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.beesang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeesangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
