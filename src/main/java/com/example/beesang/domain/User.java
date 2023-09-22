89d9911bcef152bd1a165a44b7d1474455440967
zzzzseong <jung48182@gmail.com>
2023-09-22 16:26:54 +0900

    feat(User): User domain 추가

---
+++ src/main/java/com/example/beesang/domain/User.java
@@ -0,0 +1,21 @@
+package com.example.beesang.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id @GeneratedValue
+    @Column(name = "user_id")
+    private Long id;
+
+}
