de5c8055784992cb3134cc6953abf22604447669
zzzzseong <jung48182@gmail.com>
2023-10-05 14:51:15 +0900

    !UPDATE WORKFLOW FILE FOR CI/CD

--- src/main/java/com/example/beesang/BeeSangApplication.java
+++ src/main/java/com/example/beesang/BeeSangApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BeeSangApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BeeSangApplication.class, args);
 	}
-
 }
