4e2f42340c6c2ba2a549381104fc7a4590520bea
zzzzseong <jung48182@gmail.com>
2023-10-22 20:55:47 +0900

    feat: quiz domain 속성 추가

--- src/main/java/com/example/beesang/domain/study/Quiz.java
+++ src/main/java/com/example/beesang/domain/study/Quiz.java
@@ -1,8 +1,12 @@
 package com.example.beesang.domain.study;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Getter
+@NoArgsConstructor
 public class Quiz {
 
     @Id @GeneratedValue
@@ -12,4 +16,19 @@ public class Quiz {
     @ManyToOne
     @JoinColumn(name="chapter_id")
     private Chapter chapter;
+
+    private int time;
+    private String title;
+    private String question;
+    private String description;
+    private boolean answer;
+
+    public Quiz(Chapter chapter, int time, String title, String question, String description, boolean answer) {
+        this.chapter = chapter;
+        this.time = time;
+        this.title = title;
+        this.question = question;
+        this.description = description;
+        this.answer = answer;
+    }
 }
